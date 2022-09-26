f5af1484751579fd5bf94a6a24c6162861c309af
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2022-09-26 14:32:15 -0300

    conclusão de funcionalidade de sons

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]    
+    const idAudio = `#som_${instrumento}`     
+
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    }
+    console.log(contador)
+}
